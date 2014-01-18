fc28896661f2d386103a83cef9ddd638cdbc29c1
Amir Grozki <amirgrozki@gmail.com>
2014-01-18 02:52:50 +0200

    Added support for Node.js server.

---
+++ server.js
@@ -0,0 +1,37 @@
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    express = require('express'),
+    scss = require('node-sass'),
+
+    config = require('./config.json'),
+
+    app = express(),
+    server = http.createServer(app);
+
+app.set('view engine', 'hbs');
+
+//app.use(express.logger());
+
+app.use(express.compress());
+
+app.use(express.methodOverride());
+
+app.use(express.cookieParser());
+
+app.use(express.session({
+  secret: config.secret,
+  key: config.session_key
+}));
+
+app.use(scss.middleware({
+  src: __dirname,
+  dest: __dirname,
+  debug: true,
+  outputStyle: 'compressed'
+}));
+
+app.use(express.static(__dirname));
+
+server.listen(port);
+
+console.log('Express :: Listening on port ' + port);
